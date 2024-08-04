9a5a8736d1e14331e09168304a19a4dc49a3850e
= <manni.basi@gmail.com>
2024-08-04 11:33:12 +0100

    Initial Spring Web project setup

---
+++ src/main/java/org/familycashcardapp/FamilyCashCardAppApplication.java
@@ -0,0 +1,13 @@
+package org.familycashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyCashCardAppApplication.class, args);
+    }
+
+}
