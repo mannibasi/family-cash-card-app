53d04704a07317911d4525963f53f3f88081a0a9
= <manni.basi@gmail.com>
2024-10-13 16:30:59 +0100

    Added first proper failing (non-compiling) test

--- src/test/java/org/familycashcardapp/CashCardJsonTest.java
+++ src/test/java/org/familycashcardapp/CashCardJsonTest.java
@@ -1,12 +1,20 @@
 package org.familycashcardapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 public class CashCardJsonTest {
+    @Autowired
+    private JacksonTester<CashCard> json;
+
     @Test
-    void myFirstTest() {
-        assertThat(1).isEqualTo(42);
+    void cashCardSerializationTest() {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
     }
+
 }
