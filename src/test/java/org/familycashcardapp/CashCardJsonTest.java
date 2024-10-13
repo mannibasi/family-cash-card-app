c9253f4172b66bc1deb45657c5ab80860f5b008d
= <manni.basi@gmail.com>
2024-10-13 16:44:45 +0100

    Added further assertions

--- src/test/java/org/familycashcardapp/CashCardJsonTest.java
+++ src/test/java/org/familycashcardapp/CashCardJsonTest.java
@@ -18,6 +18,9 @@ public class CashCardJsonTest {
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
     }
-
 }
