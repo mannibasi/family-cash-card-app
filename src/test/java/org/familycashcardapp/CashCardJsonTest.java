a63fd9accca837265121d356b18411f8d81181c0
= <manni.basi@gmail.com>
2024-10-13 16:26:06 +0100

    Created initial failing test

---
+++ src/test/java/org/familycashcardapp/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package org.familycashcardapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    void myFirstTest() {
+        assertThat(1).isEqualTo(42);
+    }
+}
