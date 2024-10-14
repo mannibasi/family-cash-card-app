eed00d67568b449e4ebe154425e52092a2578779
= <manni.basi@gmail.com>
2024-10-14 21:25:22 +0100

    Added failing get endpoint test

--- src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
+++ src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -1,13 +1,28 @@
 package org.familycashcardapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FamilyCashCardAppApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
 }
