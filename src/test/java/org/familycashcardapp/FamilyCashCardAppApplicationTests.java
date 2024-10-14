413af5d80e3f455da49e6676fc7df35794e55a25
= <manni.basi@gmail.com>
2024-10-14 21:47:38 +0100

    Added failing test for call with unknown cash card id

--- src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
+++ src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -33,4 +33,11 @@ class FamilyCashCardAppApplicationTests {
         Double amount = documentContext.read("$.amount");
         assertThat(amount).isEqualTo(123.45);
     }
+
+    @Test
+    void shouldNotReturnACashCardWithUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
 }
