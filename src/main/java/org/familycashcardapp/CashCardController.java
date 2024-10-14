4a15f4fc1e7ec9a0fb0eae8554100eccbed7a93d
= <manni.basi@gmail.com>
2024-10-14 21:51:01 +0100

    Used path variable check to get back to green

--- src/main/java/org/familycashcardapp/CashCardController.java
+++ src/main/java/org/familycashcardapp/CashCardController.java
@@ -2,6 +2,7 @@ package org.familycashcardapp;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById(){
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
