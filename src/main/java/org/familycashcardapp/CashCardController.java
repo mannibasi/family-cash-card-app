2c56e72d4db82bad4e47792ed7ff353b677c2a61
= <manni.basi@gmail.com>
2024-10-15 21:43:39 +0100

    Added stubbed out implementation for post endpoint

--- src/main/java/org/familycashcardapp/CashCardController.java
+++ src/main/java/org/familycashcardapp/CashCardController.java
@@ -1,10 +1,7 @@
 package org.familycashcardapp;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -25,5 +22,10 @@ public class CashCardController {
             return ResponseEntity.ok(cashCardOptional.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> create(@RequestBody CashCard cashCard) {
+        return null;
     }
 }
