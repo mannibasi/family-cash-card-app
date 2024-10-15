64efaeaf394613c7540b6ba7b2b6c6717534e0c6
= <manni.basi@gmail.com>
2024-10-15 22:24:26 +0100

    Minor refactor of filename(s)

--- src/test/java/org/familycashcardapp/CashCardJsonTest.java
+++ src/test/java/org/familycashcardapp/CashCardJsonTest.java
@@ -32,7 +32,7 @@ public class CashCardJsonTest {
     @Test
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("single.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
