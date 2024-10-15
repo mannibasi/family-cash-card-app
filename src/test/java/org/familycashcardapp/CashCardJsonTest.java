8cbac1bdc7239564e67d74fce9fda4689c38df96
= <manni.basi@gmail.com>
2024-10-15 22:21:25 +0100

    Added deliberately failing test for list serialization

--- src/test/java/org/familycashcardapp/CashCardJsonTest.java
+++ src/test/java/org/familycashcardapp/CashCardJsonTest.java
@@ -1,5 +1,7 @@
 package org.familycashcardapp;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -13,6 +15,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CashCardJsonTest {
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.00),
+                new CashCard(101L, 150.00));
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
@@ -35,5 +50,10 @@ public class CashCardJsonTest {
         assertThat(json.parse(content)).isEqualTo(new CashCard(99L, 123.45));
         assertThat(json.parseObject(content).id()).isEqualTo(99L);
         assertThat(json.parseObject(content).amount()).isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
     }
 }
