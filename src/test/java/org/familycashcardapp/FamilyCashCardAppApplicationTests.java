218bc5dbdaa7ee6369e87f333227a98a18a2d99e
Manni Basi <manni.basi@gmail.com>
2024-11-02 03:51:58 +0000

    Added failing test RE default sort params

--- src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
+++ src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -102,4 +102,17 @@ class FamilyCashCardAppApplicationTests {
         double amount = documentContext.read("$[0].amount");
         assertThat(amount).isEqualTo(150.00);
     }
+
+    @Test
+    void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        JSONArray read = documentContext.read("$[*]");
+        assertThat(read.size()).isEqualTo(3);
+
+        JSONArray amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
+    }
 }
