f135f3fe7bd6d0b77b6a000051c416205f66b801
Manni Basi <manni.basi@gmail.com>
2024-11-03 07:17:11 +0000

    Ensure record has been deleted (is not found)

--- src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
+++ src/test/java/org/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -186,5 +186,8 @@ class FamilyCashCardAppApplicationTests {
     void shouldDeleteAnExistingCashCard() {
         ResponseEntity<Void> response = restTemplate.withBasicAuth("manni", "testUserPassword123!").exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate.withBasicAuth("manni", "testUserPassword123!").getForEntity("/cashcards/99", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
